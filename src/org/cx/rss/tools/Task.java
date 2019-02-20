ea1930fd54be0681681082c2c12c56c64fe6bd5e
chenxian <228525125@qq.com>
2019-02-20 16:05:46 +0800

    改为每小时发送一次邮件

--- src/org/cx/rss/tools/Task.java
+++ src/org/cx/rss/tools/Task.java
@@ -30,7 +30,7 @@ public class Task extends TimerTask {
 		IKingdeeService ks = (IKingdeeService) context.getBean("kingdeeService");
 		Date date = new Date(System.currentTimeMillis());
 		String uri = sc.getInitParameter("PublicNetworkIPSite");
-		if(7 == date.getHours()){       // 早上7点执行
+		/*if(7 == date.getHours()){       // 早上7点执行
 			String resutl = HttpClient.getPublicNetworkIP(uri);
 			SendMailUtil.sendQQMail("PublicNetworkIP", resutl);
 			SimpleDateFormat df = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
@@ -49,11 +49,15 @@ public class Task extends TimerTask {
 			SendMailUtil.sendQQMail("PublicNetworkIP", resutl);
 			SimpleDateFormat df = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
 			System.out.println("task time:"+df.format(date)+"send mail");
-		}
+		}*/
 		
 		ks.exec_close_scrw();
 		SimpleDateFormat df = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
 		System.out.println("task time:"+df.format(date)+"exec close_scrw");
+		
+		String resutl = HttpClient.getPublicNetworkIP(uri);
+		SendMailUtil.sendQQMail("PublicNetworkIP", resutl);
+		System.out.println("task time:"+df.format(date)+"send mail");
 	}
 	
 	public static void main(String[] args) {
