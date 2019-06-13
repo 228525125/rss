b2908e428ebca795f57806480670136650ea4c81
chenxian <228525125@qq.com>
2019-06-13 09:55:33 +0800

    1、修改 外协送检异常 问题

--- WebContent/plugins/extjs/rss/kingdee/list_wxsjyc.js
+++ WebContent/plugins/extjs/rss/kingdee/list_wxsjyc.js
@@ -1,5 +1,5 @@
-/**
- * 外协及时率明细表
+﻿/**
+ * 外协送检异常
  */
 WxsjycListPanel = Ext.extend(Ext.Panel, {
     layout: 'fit',
@@ -9,10 +9,10 @@ WxsjycListPanel = Ext.extend(Ext.Panel, {
 	
 		//统计报表数据
 		this.store = new Ext.data.JsonStore({
-			url:'kingdee.do?cmd=list_wxsjyc',
+			url:'kingdee.do?cmd=list_wwjywzdj_hz',
 			root: 'result',
 			totalProperty:"rowCount",
-			fields:['wldm','wlmc','wlgg','sjsl','jysl'],
+			fields:['djbh','djrq','jhrq','dhrq','cpdm','cpmc','cpgg','cpth','jldw','djsl','fssl','sjsl','jysl','hgsl','jyfs','jgdw','jssl'],
 	  		remoteSort:true,
 	  		baseParams:{pageSize:2000}
 	    });
@@ -45,41 +45,111 @@ WxsjycListPanel = Ext.extend(Ext.Panel, {
             	        }),                       
                     	colModel: new Ext.ux.grid.LockingColumnModel([
                     	    new Ext.grid.RowNumberer(),
-                            {
+                    	    {
                                 xtype: 'gridcolumn',
-                                header: '物料代码',
-                                sortable: true,
+                                header: '单据编号',
                                 resizable: true,
                                 width: 100,
-                                dataIndex: 'wldm'
+                                dataIndex: 'djbh'
                             },{
+                                xtype: 'gridcolumn',
+                                header: '申请日期',
+                                resizable: true,
+                                width: 80,
+                                dataIndex: 'djrq'
+                            },{
+                                xtype: 'gridcolumn',
+                                header: '交货日期',
+                                resizable: true,
+                                width: 80,
+                                dataIndex: 'jhrq'
+                            },{
+                                xtype: 'gridcolumn',
+                                header: '检验方式',
+                                resizable: true,
+                                width: 40,
+                                dataIndex: 'jyfs'
+                            },{
+                                xtype: 'gridcolumn',
+                                header: '物料代码',
+                                resizable: true,
+                                width: 80,
+                                dataIndex: 'cpdm'
+                            },
+                            {
                                 xtype: 'gridcolumn',
                                 header: '物料名称',
-                                sortable: true,
                                 resizable: true,
                                 width: 150,
-                                dataIndex: 'wlmc'
+                                renderer:function(value,metadata,record){
+	          		    	  		return value;
+	          		      		},
+                                dataIndex: 'cpmc'
+                            },
+                            {
+                                xtype: 'gridcolumn',
+                                header: '图号',
+                                resizable: true,
+                                width: 80,
+                                dataIndex: 'cpth'
                             },{
                                 xtype: 'gridcolumn',
-                                header: '物料规格',
-                                sortable: true,
+                                header: '规格',
                                 resizable: true,
-                                width: 150,
-                                dataIndex: 'wlgg'
+                                width: 80,
+                                dataIndex: 'cpgg'
+                            },{
+                                xtype: 'gridcolumn',
+                                header: '单位',
+                                resizable: true,
+                                width: 40,
+                                dataIndex: 'jldw'
+                            },{
+                                xtype: 'gridcolumn',
+                                header: '待检数量',
+                                resizable: true,
+                                width: 60,
+                                dataIndex: 'djsl',
+                                renderer:function(value,metadata,record){	                            	
+	                        		return value;
+	                        	}
+                            },{
+                                xtype: 'gridcolumn',
+                                header: '加工单位',
+                                resizable: true,
+                                width: 40,
+                                dataIndex: 'jgdw'
+                            },{
+                                xtype: 'gridcolumn',
+                                header: '转出数量',
+                                resizable: true,
+                                width: 40,
+                                dataIndex: 'fssl'
                             },{
                                 xtype: 'gridcolumn',
                                 header: '送检数量',
-                                sortable: true,
                                 resizable: true,
-                                width: 80,
+                                width: 40,
                                 dataIndex: 'sjsl'
                             },{
                                 xtype: 'gridcolumn',
                                 header: '检验数量',
-                                sortable: true,
                                 resizable: true,
-                                width: 80,
+                                hidden : true,
+                                width: 40,
                                 dataIndex: 'jysl'
+                            },{
+                                xtype: 'gridcolumn',
+                                header: '合格数量',
+                                resizable: true,
+                                width: 40,
+                                dataIndex: 'hgsl'
+                            },{
+                                xtype: 'gridcolumn',
+                                header: '接收数量',
+                                resizable: true,
+                                width: 40,
+                                dataIndex: 'jssl'
                             }
                         ])
                     }
